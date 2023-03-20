b7f5431fe2fe3b2ad30b448208b9bf55b532588e
pablo <pnunfe@gmail.com>
2023-03-20 22:55:05 +0000

    112 StateFlow added

--- app/src/main/java/com/example/recado/addtasks/ui/TasksViewModel.kt
+++ app/src/main/java/com/example/recado/addtasks/ui/TasksViewModel.kt
@@ -5,24 +5,42 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.recado.addtasks.domain.AddTaskUseCase
+import com.example.recado.addtasks.domain.GetTaksUseCase
+import com.example.recado.addtasks.ui.TasksUiState.*
 import com.example.recado.addtasks.ui.model.TaskModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
-class TasksViewModel @Inject constructor():ViewModel() {
+class TasksViewModel @Inject constructor(
+    private val addTaskUseCase: AddTaskUseCase,
+    getTasksUseCase: GetTaksUseCase
+) : ViewModel() {
+
+    val uiState: StateFlow<TasksUiState> = getTasksUseCase().map(::Success).catch {
+        Error(it)
+    }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), Loading)
 
     private val _showDialog = MutableLiveData<Boolean>()
-    val showDialog:LiveData<Boolean> = _showDialog
+    val showDialog: LiveData<Boolean> = _showDialog
 
     private val _tasks = mutableStateListOf<TaskModel>()
     val task: List<TaskModel> = _tasks
 
     fun onDialogClose() {
-        _showDialog.value = false    }
+        _showDialog.value = false
+    }
 
     fun onTaskCreated(task: String) {
         _showDialog.value = false
-        _tasks.add(TaskModel(task))
+        _tasks.add(TaskModel(task = task))
+        viewModelScope.launch {
+            addTaskUseCase(TaskModel(task = task))
+        }
     }
 
     fun onShowDialogClick() {
